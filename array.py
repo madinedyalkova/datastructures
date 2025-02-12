6844c7ce3837c05b81db92fae80564990bf9f7b3
Madlen N. Nedyalkova <madlennedyalkova@gmail.com>
2025-02-12 12:43:29 +0200

    array.py

---
+++ array.py
@@ -0,0 +1,12 @@
+string = ['m','n','j','p']
+print(strings[2])
+
+string.append('e')      # O(1)
+
+string.pop()            # O(1)
+
+string.insert(0,'x')    #O(n)
+
+string.insert(2,'alien')   #O(n)
+
+print(string)
